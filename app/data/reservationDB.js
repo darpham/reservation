3e0895f33f4c1b7dff103aaa223c308972e0d2e6
Darren Pham <phamdarren1@gmail.com>
2019-06-05 19:40:18 -0700

    moved js for mysql db creation to app/data

---
+++ app/data/reservationDB.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const app = require('express')
+const inquirer = require('inquirer')
+const fs = require('fs')
+const mysql = require('mysql')
+const readline = require('readline')
+
+inquirer
+    .prompt([{
+        message: 'Please enter your MySQL username',
+        name: 'username'
+    },
+    {
+        message: 'Please enter your MySQL password',
+        name: 'password',
+        type: 'password'
+    }])
+    .then(answers => {
+        
+        const myCon = mysql.createConnection({
+            host: 'localhost',
+            port: 3000,
+            user: answers.username,
+            password: answers.password
+        })
+
+        var rl = readline.createInterface({
+            input: fs.createReadStream('./app/data/reservations.sql'),
+            terminal: false
+           });
+
+        rl.on('line', function(chunk){
+            myCon.query(chunk.toString('ascii'), function(err, sets, fields){
+            if(err) console.log(err);
+            });
+        });
+
+        rl.on('close', function(){
+        console.log("finished");
+        myCon.end();
+        });
+    });
